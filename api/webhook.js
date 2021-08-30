8b943df2645bbb9057acfb6f0ad118c45eb13c5f
Sam Kozin <sam.kozin@gmail.com>
2021-08-30 17:34:27 +0300

    add token auth

--- api/webhook.js
+++ api/webhook.js
@@ -2,12 +2,17 @@ import fetch from 'node-fetch'
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 const CHAT_ID = process.env.CHAT_ID
+const AUTH_TOKEN = process.env.AUTH_TOKEN
 
 export default async (req, res) => {
+  if (req.query.token !== AUTH_TOKEN) {
+    res.status(401).send('Unauthorized')
+    return
+  }
   try {
     const text = formatMessage(req.body)
     await postUpdate(text)
-    res.send('ok')
+    res.send('Ok')
   } catch (err) {
     res.status(500).send(`error: ${err.stack}`)
   }
